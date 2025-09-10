d951aacdad1eab0a03549258f823158d9f9a395e
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-09-10 23:47:59 +0200

    Anchor score label to the viewport

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,9 @@
 extends Node
 
-@onready var score_label = $ScoreLabel
+@export var score_label: Label
 
 var score = 0
 
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins"
-
-
+	score_label.text = "Coins: " + str(score)
